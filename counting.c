3398b5d9df67c48aebbc72762867998ad9af1615
Archnakushwaha2008 <archnakushwaha2008@gmail.com>
2025-05-08 06:09:23 -0700

    Add files via upload

---
+++ counting.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+//	1 to 100 cuouting
+	int i;
+	for(i = 1; i <= 100; i++) {
+	
+	printf("%d\t", i);
+	
+	}
+//	100 to 1 counting
+	int a;
+	for(a = 100; a >= 1; a--) {
+	
+	printf("%d\t", a);
+	
+	}
+	
+
+	
+	
+	return 0;
+}
